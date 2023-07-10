cd155bf531c86e5503c4caee266146d3f41c0e44
Yaroslav Pashynskyi <yaroslav.pashynskyi@gmail.com>
2023-07-10 15:43:04 +0300

    Add product function for testing

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Product } from "../models/Product";
+import Catalog from "../../features/Catalog/Catalog";
+import { Typography } from "@mui/material";
 
 function App() {
   const [products, setProducts] = useState<Product[]>([])
@@ -9,14 +11,22 @@ function App() {
       .then(response => response.json())
       .then(data => setProducts(data))
   }, [])
+
+  function addProduct() {
+    setProducts(prevState => [...prevState,
+    {
+      id: prevState.length + 101,
+      name: "product" + (prevState.length + 1),
+      price: (prevState.length * 100) + 100,
+      brand: "some brand",
+      description: "some desc",
+      pictureUrl: "http://picsum.photos/200"
+    }])
+  }
   return (
     <div>
-      <h1>Ski Store</h1>
-      <ul>
-        {products.map((item) => (
-          <li key={item.id}>{item.name} - {item.price}</li>
-        ))}
-      </ul>
+      <Typography variant="h1">Ski Store</Typography>
+      <Catalog products={products} addProduct={addProduct} />
     </div>
   );
 }
