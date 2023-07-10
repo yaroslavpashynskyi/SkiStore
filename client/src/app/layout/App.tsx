ea1156fbd29170cf25d97d6f0f85afbaaa5eb8ab
Yaroslav Pashynskyi <yaroslav.pashynskyi@gmail.com>
2023-07-10 18:03:10 +0300

    Add darkmode state and theme provider

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,33 +1,31 @@
 import React, { useEffect, useState } from "react";
-import { Product } from "../models/Product";
 import Catalog from "../../features/Catalog/Catalog";
-import { Typography } from "@mui/material";
+import Header from "./Header";
+import { Container, CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 
 function App() {
-  const [products, setProducts] = useState<Product[]>([])
+  const [darkMode, setDarkMode] = useState(false);
+  const paletteType = darkMode ? "dark" : "light"
+  const theme = createTheme({
+    palette: {
+      mode: paletteType,
+      background: {
+        default: paletteType === "light" ? "#eaeaea" : "#121212"
+      }
+    }
+  })
 
-  useEffect(() => {
-    fetch("http://localhost:5000/api/products")
-      .then(response => response.json())
-      .then(data => setProducts(data))
-  }, [])
-
-  function addProduct() {
-    setProducts(prevState => [...prevState,
-    {
-      id: prevState.length + 101,
-      name: "product" + (prevState.length + 1),
-      price: (prevState.length * 100) + 100,
-      brand: "some brand",
-      description: "some desc",
-      pictureUrl: "http://picsum.photos/200"
-    }])
+  function handleThemeChange() {
+    setDarkMode(!darkMode);
   }
   return (
-    <div>
-      <Typography variant="h1">Ski Store</Typography>
-      <Catalog products={products} addProduct={addProduct} />
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
+      <Container >
+        <Catalog />
+      </Container>
+    </ThemeProvider>
   );
 }
 
