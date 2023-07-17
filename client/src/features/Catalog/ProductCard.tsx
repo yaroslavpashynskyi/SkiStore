60a88c63160c6f068ac4c4a666ea2e895764e44a
Yaroslav Pashynskyi <yaroslav.pashynskyi@gmail.com>
2023-07-17 19:58:44 +0300

    Instead button add action area for product details

--- client/src/features/Catalog/ProductCard.tsx
+++ client/src/features/Catalog/ProductCard.tsx
@@ -1,5 +1,6 @@
-import { Avatar, Button, Card, CardActions, CardContent, CardHeader, CardMedia, Typography } from "@mui/material"
+import { Avatar, Button, Card, CardActionArea, CardActions, CardContent, CardHeader, CardMedia, Typography } from "@mui/material"
 import { Product } from "../../app/models/Product"
+import { Link } from "react-router-dom"
 
 interface Props {
   product: Product
@@ -18,22 +19,23 @@ export default function ProductCard({ product }: Props) {
           sx: { fontWeight: "bold", color: "primary.main" }
         }}
       />
-      <CardMedia
-        sx={{ height: 140, backgroundSize: "contain", bgcolor: "primary.light" }}
-        image={product.pictureUrl}
-        title={product.name}
-      />
-      <CardContent>
-        <Typography gutterBottom color="secondary" variant="h5">
-          ${(product.price / 100).toFixed(2)}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {product.brand} / {product.type}
-        </Typography>
-      </CardContent>
+      <CardActionArea component={Link} to={`/catalog/${product.id}`}>
+        <CardMedia
+          sx={{ height: 140, backgroundSize: "contain", bgcolor: "primary.light" }}
+          image={product.pictureUrl}
+          title={product.name}
+        />
+        <CardContent>
+          <Typography gutterBottom color="secondary" variant="h5">
+            ${(product.price / 100).toFixed(2)}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {product.brand} / {product.type}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
       <CardActions>
         <Button size="small">Add to cart</Button>
-        <Button size="small">View</Button>
       </CardActions>
     </Card>
   )
