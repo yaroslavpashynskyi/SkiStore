4de3596a224d80df75a978bcd0cdb3c76f5bc6c7
Yaroslav Pashynskyi <yaroslav.pashynskyi@gmail.com>
2023-07-20 17:46:08 +0300

    Add exception handling middleware

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using API.Data;
+using API.MiddleWare;
 
 using Microsoft.EntityFrameworkCore;
 
@@ -21,6 +22,8 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleWare>();
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
