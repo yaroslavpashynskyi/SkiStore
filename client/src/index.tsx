7ed353f94d5c230024f7158311666b1b6bf91c86
Yaroslav Pashynskyi <yaroslav.pashynskyi@gmail.com>
2023-08-04 20:06:14 +0300

    Using React context to centralise state

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -8,13 +8,16 @@ import './app/layout/styles.css';
 import reportWebVitals from './reportWebVitals';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './app/router/Routes';
+import { StoreProvider } from './app/context/StoreContext';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <StoreProvider>
+      <RouterProvider router={router} />
+    </StoreProvider>
   </React.StrictMode>
 );
 
