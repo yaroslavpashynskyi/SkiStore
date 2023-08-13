02c4380a4a0d2b4b50d4699a6be293861fcfbeb0
Yaroslav Pashynskyi <yaroslav.pashynskyi@gmail.com>
2023-08-13 21:00:01 +0300

    Create basic counter with redux and redux toolkit

--- client/src/features/contact/ContactPage.tsx
+++ client/src/features/contact/ContactPage.tsx
@@ -1,9 +1,23 @@
-import { Typography } from "@mui/material";
+import { Button, ButtonGroup, Typography } from "@mui/material";
+import { useAppDispatch, useAppSelector } from "../../app/store/configureStore";
+import { decrement, increment } from "./counterSlice";
 
 export default function ContactPage() {
+  const dispatch = useAppDispatch();
+  const { data, title } = useAppSelector((state) => state.counter)
   return (
-    <Typography variant="h2">
-      Contact Page
-    </Typography>
+    <>
+      <Typography variant="h2">
+        {title}
+      </Typography>
+      <Typography variant="h5">
+        The data is {data}
+      </Typography>
+      <ButtonGroup>
+        <Button onClick={() => dispatch(decrement(1))} variant="contained" color="error">Decrement counter</Button>
+        <Button onClick={() => dispatch(increment(1))} variant="contained" color="primary">Increment counter</Button>
+        <Button onClick={() => dispatch(increment(5))} variant="contained" color="primary">Increment counter by 5</Button>
+      </ButtonGroup>
+    </>
   )
 }
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -9,6 +9,8 @@ import reportWebVitals from './reportWebVitals';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './app/router/Routes';
 import { StoreProvider } from './app/context/StoreContext';
+import { Provider } from 'react-redux';
+import { store } from './app/store/configureStore';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -16,7 +18,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <StoreProvider>
-      <RouterProvider router={router} />
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
     </StoreProvider>
   </React.StrictMode>
 );
