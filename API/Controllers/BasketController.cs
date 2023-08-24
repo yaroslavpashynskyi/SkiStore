a8c7f4b906da5f3ccf39179d3cbd6854c62ce2dd
Yaroslav Pashynskyi <yaroslav.pashynskyi@gmail.com>
2023-08-24 12:10:06 +0300

    If product missing in the basket throw bad request

--- API/Controllers/BasketController.cs
+++ API/Controllers/BasketController.cs
@@ -39,7 +39,7 @@ namespace API.Controllers
 
             var product = await _context.Products.FindAsync(productId);
             if (product == null)
-                return NotFound();
+                return BadRequest(new ProblemDetails { Title = "Product Not Found" });
 
             basket.AddItem(product, quantity);
             var result = await _context.SaveChangesAsync() > 0;
