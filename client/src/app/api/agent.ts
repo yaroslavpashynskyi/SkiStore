e5908a4e0ee92a33d9205f743f75297e572bcd63
Yaroslav Pashynskyi <yaroslav.pashynskyi@gmail.com>
2023-09-04 14:52:53 +0300

    Include pagination headers to the response

--- client/src/app/api/agent.ts
+++ client/src/app/api/agent.ts
@@ -1,6 +1,7 @@
 import axios, { AxiosError, AxiosResponse } from "axios";
 import { toast } from "react-toastify";
 import { router } from "../router/Routes";
+import { PaginatedResponse } from "../models/pagination";
 
 axios.defaults.baseURL = "http://localhost:5000/api/";
 axios.defaults.withCredentials = true;
@@ -12,6 +13,11 @@ const sleep = () => new Promise(resolve => setTimeout(resolve, 500));
 axios.interceptors.response.use(
     async response => {
         await sleep();
+        const pagination = response.headers["pagination"];
+        if (pagination) {
+            response.data = new PaginatedResponse(response.data, JSON.parse(pagination));
+            return response;
+        }
         return response;
     },
     (error: AxiosError) => {
