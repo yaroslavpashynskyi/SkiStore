4506fce04278f525c783354a47661ff1ade38fbe
Yaroslav Pashynskyi <yaroslav.pashynskyi@gmail.com>
2023-11-29 20:21:51 +0200

    Handle successful payment

--- client/src/features/checkout/CheckoutWrapper.tsx
+++ client/src/features/checkout/CheckoutWrapper.tsx
@@ -1,12 +1,28 @@
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import CheckoutPage from "./CheckoutPage";
+import { useAppDispatch } from "../../app/store/configureStore";
+import { useEffect, useState } from "react";
+import agent from "../../app/api/agent";
+import { setBasket } from "../basket/basketSlice";
+import LoadingComponent from "../../app/layout/LoadingComponent";
 
 const stripePromise = loadStripe('pk_test_51NtvDHJOQdR9i9fC85kKE2ypCDCWNtRyE2AsSZtlkwLZRi5zCfvRorYYw5xT2hzGStaXxInleFp2eBzuQnUDg7sm009J4SU2OZ', { locale: 'en' });
 
 
 
 export default function CheckoutWrapper() {
+  const dispatch = useAppDispatch();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    agent.Payments.createPaymentIntent()
+      .then(basket => dispatch(setBasket(basket)))
+      .catch(error => console.log(error))
+      .finally(() => setLoading(false))
+  }, [dispatch])
+
+  if (loading) return <LoadingComponent message="Loading checkout..." />
 
   return (
     <Elements stripe={stripePromise}>
