f3476a3777f5977036bad263d2afa72653ead4a8
Yaroslav Pashynskyi <yaroslav.pashynskyi@gmail.com>
2023-12-02 23:42:26 +0200

    Migrate from Create React App to Vite

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,7 +5,6 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import ReactDOM from 'react-dom/client';
 import './app/layout/styles.css';
-import reportWebVitals from './reportWebVitals';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './app/router/Routes';
 import { Provider } from 'react-redux';
@@ -23,8 +22,3 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
