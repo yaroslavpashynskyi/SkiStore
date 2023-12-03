dce1e0d826cd2393ee68efa8cf63ab60c2eb8b77
Yaroslav Pashynskyi <yaroslav.pashynskyi@gmail.com>
2023-12-03 18:40:34 +0200

    Switch to postgres database

--- API/Program.cs
+++ API/Program.cs
@@ -17,7 +17,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<StoreContext>(opt =>
 {
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddControllers();
 builder.Services
