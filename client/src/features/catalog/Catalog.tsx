1267784c5bb3b34ff933be528d74328461a53e2c
Yaroslav Pashynskyi <yaroslav.pashynskyi@gmail.com>
2023-12-08 23:31:45 +0200

    Create products hook

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,13 +1,13 @@
 import LoadingComponent from "../../app/layout/LoadingComponent"
 import { useAppDispatch, useAppSelector } from "../../app/store/configureStore"
 import ProductList from "./ProductList"
-import { useEffect } from "react"
-import { fetchFiltersAsync, fetchProductsAsync, productSelectors, setProductParams } from "./catalogSlice"
+import { setProductParams } from "./catalogSlice"
 import { Grid, Paper } from "@mui/material"
 import ProductSearch from "./ProductSearch"
 import RadioButtonGroup from "../../app/components/RadioButtonGroup"
 import CheckboxButtons from "../../app/components/CheckboxButtons"
 import AppPagination from "../../app/components/AppPagination"
+import useProducts from "../../app/hooks/useProducts"
 
 const sortOptions = [
   { value: "name", label: "Alphabetical" },
@@ -16,17 +16,10 @@ const sortOptions = [
 ]
 
 export default function Catalog() {
-  const products = useAppSelector(productSelectors.selectAll)
-  const { productsLoaded, filtersLoaded, brands, types, productParams, metaData } = useAppSelector(state => state.catalog)
+  const { products, brands, types, filtersLoaded, metaData } = useProducts();
   const dispatch = useAppDispatch();
 
-  useEffect(() => {
-    if (!productsLoaded) dispatch(fetchProductsAsync());
-  }, [dispatch, productsLoaded]);
-
-  useEffect(() => {
-    if (!filtersLoaded) dispatch(fetchFiltersAsync());
-  }, [dispatch, filtersLoaded]);
+  const { productParams } = useAppSelector(state => state.catalog)
 
   if (!filtersLoaded) return <LoadingComponent message="Loading products..." />
 
