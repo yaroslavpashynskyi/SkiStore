cdff510434a78e24a8dd7b94d21ae5edebb4f0b4
Yaroslav Pashynskyi <yaroslav.pashynskyi@gmail.com>
2023-12-08 23:30:44 +0200

    More responsiveness to pagination button

--- client/src/app/components/AppPagination.tsx
+++ client/src/app/components/AppPagination.tsx
@@ -1,5 +1,6 @@
 import { Box, Typography, Pagination } from "@mui/material";
 import { MetaData } from "../models/pagination";
+import { useState } from "react";
 
 interface Props {
   metaData: MetaData;
@@ -7,6 +8,12 @@ interface Props {
 }
 export default function AppPagination({ metaData, onPageChange }: Props) {
   const { currentPage, totalCount, totalPages, pageSize } = metaData;
+  const [pageNumber, setPageNumber] = useState(currentPage);
+
+  function handlePageChange(page: number) {
+    setPageNumber(page);
+    onPageChange(page);
+  }
 
   return (
     <Box display={"flex"} justifyContent={"space-between"} alignItems={"center"}>
@@ -19,9 +26,9 @@ export default function AppPagination({ metaData, onPageChange }: Props) {
         color="secondary"
         size="large"
         count={totalPages}
-        page={currentPage}
+        page={pageNumber}
         onChange={(_event, page) => {
-          onPageChange(page);
+          handlePageChange(page)
         }}
       />
     </Box>
