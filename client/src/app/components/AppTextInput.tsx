9bbad682c6f913b65bcb6d230999a4e8cddf7840
Yaroslav Pashynskyi <yaroslav.pashynskyi@gmail.com>
2023-12-17 22:03:15 +0200

    Reusable text area and number inputs

--- client/src/app/components/AppTextInput.tsx
+++ client/src/app/components/AppTextInput.tsx
@@ -2,7 +2,10 @@ import { TextField } from "@mui/material";
 import { UseControllerProps, useController } from "react-hook-form";
 
 interface Props extends UseControllerProps {
-  label: string;
+  label: string
+  multiline?: boolean
+  rows?: number
+  type?: string
 }
 
 export default function AppTextInput(props: Props) {
@@ -13,6 +16,9 @@ export default function AppTextInput(props: Props) {
       {...field}
       fullWidth
       variant="outlined"
+      multiline={props.multiline}
+      rows={props.rows}
+      type={props.type}
       error={!!fieldState.error}
       helperText={fieldState.error?.message}
     />
