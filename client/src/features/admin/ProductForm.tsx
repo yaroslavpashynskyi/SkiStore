f187b93ba096dcde9bef8cdb34e0f1b988a02733
Yaroslav Pashynskyi <yaroslav.pashynskyi@gmail.com>
2023-12-17 22:32:26 +0200

    Use app reusable components in product form

--- client/src/features/admin/ProductForm.tsx
+++ client/src/features/admin/ProductForm.tsx
@@ -1,15 +1,24 @@
 import { Typography, Grid, Paper, Box, Button } from "@mui/material";
-import { useForm } from "react-hook-form";
+import { FieldValues, useForm } from "react-hook-form";
 import AppTextInput from "../../app/components/AppTextInput";
 import { Product } from "../../app/models/product";
 import { useEffect } from "react";
+import useProducts from "../../app/hooks/useProducts";
+import AppSelectList from "../../app/components/AddSelectList";
+import AppDropzone from "../../app/components/AppDropzone";
 
 interface Props {
   product?: Product;
   cancelEdit: () => void;
 }
 export default function ProductForm({ product, cancelEdit }: Props) {
-  const { control, reset } = useForm();
+  const { control, reset, handleSubmit, watch } = useForm();
+  const { brands, types } = useProducts();
+  const watchFile = watch('file', null);
+
+  function handleSubmitData(data: FieldValues) {
+    console.log(data);
+  }
 
   useEffect(() => {
     if (product) reset(product);
@@ -20,33 +29,42 @@ export default function ProductForm({ product, cancelEdit }: Props) {
       <Typography variant="h4" gutterBottom sx={{ mb: 4 }}>
         Product Details
       </Typography>
-      <Grid container spacing={3}>
-        <Grid item xs={12} sm={12}>
-          <AppTextInput control={control} name='name' label='Product name' />
+      <form onSubmit={handleSubmit(handleSubmitData)}>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={12}>
+            <AppTextInput control={control} name='name' label='Product name' />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <AppSelectList control={control} items={brands} name='brand' label='Brand' />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <AppSelectList control={control} items={types} name='type' label='Type' />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <AppTextInput type="number" control={control} name='price' label='Price' />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <AppTextInput type="number" control={control} name='quantityInStock' label='Quantity in Stock' />
+          </Grid>
+          <Grid item xs={12}>
+            <AppTextInput multiline={true} rows={4} control={control} name='description' label='Description' />
+          </Grid>
+          <Grid item xs={12}>
+            <Box display="flex" justifyContent="space-between" alignItems="center">
+              <AppDropzone control={control} name='file' />
+              {watchFile ? (
+                <img src={watchFile.preview} alt="preview" style={{ maxHeight: 200 }} />
+              ) : (
+                <img src={product?.pictureUrl} alt={product?.name} style={{ maxHeight: 200 }} />
+              )}
+            </Box>
+          </Grid>
         </Grid>
-        <Grid item xs={12} sm={6}>
-          <AppTextInput control={control} name='brand' label='Brand' />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <AppTextInput control={control} name='type' label='Type' />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <AppTextInput control={control} name='price' label='Price' />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <AppTextInput control={control} name='quantityInStock' label='Quantity in Stock' />
-        </Grid>
-        <Grid item xs={12}>
-          <AppTextInput control={control} name='description' label='Description' />
-        </Grid>
-        <Grid item xs={12}>
-          <AppTextInput control={control} name='pictureUrl' label='Image' />
-        </Grid>
-      </Grid>
-      <Box display='flex' justifyContent='space-between' sx={{ mt: 3 }}>
-        <Button onClick={cancelEdit} variant='contained' color='inherit'>Cancel</Button>
-        <Button variant='contained' color='success'>Submit</Button>
-      </Box>
+        <Box display='flex' justifyContent='space-between' sx={{ mt: 3 }}>
+          <Button onClick={cancelEdit} variant='contained' color='inherit'>Cancel</Button>
+          <Button type="submit" variant='contained' color='success'>Submit</Button>
+        </Box>
+      </form >
     </Box>
   )
 }
